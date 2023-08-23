4954e897282722961189abe50cee8986cdd89f70
bexruz <sobirovbehruz4915@gmail.com>
2023-08-23 15:57:09 +0500

    it have been updated by Bexruz

--- src/uz/pdp/Main.java
+++ src/uz/pdp/Main.java
@@ -4,6 +4,7 @@ import src.uz.pdp.config.MailConfig;
 import src.uz.pdp.util.MailUtils;
 
 import javax.mail.Session;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -13,7 +14,7 @@ public class Main {
     private static int id=0;
     private static List<User> dataBase=new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("1.register\n2.get db");
+        System.out.println("1.register\n2.get db\nsend file");
         int r=sc().nextInt();
         switch (r){
             case 1->{
@@ -46,6 +47,22 @@ public class Main {
                 for (int i = 0; i < id; i++) {
                     System.out.println(dataBase.get(i));
                 }
+            }case 3->{
+                Session session= MailConfig.initAuthSession();
+                System.out.println("Ismingizni kiriting: ");
+                String ism=sc().next();
+                System.out.println("Emailingizni kiritng: ");
+                String email=sc().next();
+                Random random=new Random();
+                int n=random.nextInt(1000,9999);
+                String num=String.valueOf(n);
+                String toMsg="Dear "+ism+", it's your code \uD83D\uDC47\uD83D\uDC47\uD83D\uDC47\uD83D\uDC47 \n\n\n\n\n"+num;
+                MailUtils.sendFile(session,
+                        "no auth mailing"
+                        ,"bsobirov8027@gmail.com",email
+                        ,
+                        toMsg,new File("src/source/text.txt"));
+                System.out.println("sucessfully");
             }
         }
 
--- src/uz/pdp/util/MailUtils.java
+++ src/uz/pdp/util/MailUtils.java
@@ -2,8 +2,14 @@ package src.uz.pdp.util;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
 import java.util.logging.Logger;
+import java.util.Properties;
+import javax.activation.*;
+import javax.mail.*;
+import javax.mail.internet.MimeMultipart;
 
 public class MailUtils {
     private static final Logger log = Logger.getLogger(MailUtils.class.getName());
@@ -28,5 +34,32 @@ public class MailUtils {
         } catch (MessagingException e) {
             log.severe("Sending message error: "+e.getMessage());
         }
+    }
+    public static void sendFile(Session session, String subject, String from, String to,String text, File file){
+        Message message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(text);
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+            Multipart multipart = new MimeMultipart();
+            DataSource source = new FileDataSource(file);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(source.getName());
+            multipart.addBodyPart(messageBodyPart);
+
+
+            message.setContent(multipart);
+        } catch (MessagingException e) {
+            log.severe("Creating MimeMessage error: "+e.getMessage());
+        }
+        try {
+            Transport.send(message);
+        } catch (MessagingException e) {
+            log.severe("Sending message error: "+e.getMessage());
+        }
+
     }
 }
